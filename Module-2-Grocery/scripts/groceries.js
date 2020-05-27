4e40c1d14ce020e41566ee4f90bd6219a89e5ac0
Athavan1919 <ajeev047@uottawa.ca>
2020-05-26 21:42:36 -0400

    finished

--- Module-2-Grocery/scripts/groceries.js
+++ Module-2-Grocery/scripts/groceries.js
@@ -7,20 +7,72 @@ var products = [
 		name: "brocoli",
 		vegetarian: true,
 		glutenFree: true,
-		price: 1.99
+		price: 1.99,
+        organic: true
 	},
 	{
 		name: "bread",
 		vegetarian: true,
 		glutenFree: false,
-		price: 2.35
+		price: 2.35,
+        organic: false
 	},
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
-		price: 10.00
-	}
+		price: 10.00,
+        organic: false
+	},
+    {
+        name: "carrots",
+        vegetarian: true,
+        glutenFree: true,
+        price: 1.50,
+        organic: true
+    },
+    {
+        name: "chicken",
+        vegetarian: false,
+        glutenFree: true,
+        price: 7.99,
+        organic: true
+    },
+    {
+        name: "turkey",
+        vegetarian: false,
+        glutenFree: true,
+        price: 8.99,
+        organic: false
+    },
+    {
+        name: "lamb",
+        vegetarian: false,
+        glutenFree: true,
+        price: 5.75,
+        organic: false
+    },
+    {
+        name: "potatoes",
+        vegetarian: true,
+        glutenFree: true,
+        price: 3.50,
+        organic: true
+    },
+    {
+        name: "oats",
+        vegetarian: true,
+        glutenFree: false,
+        price: 2.50,
+        organic: false
+    },
+    {
+        name: "fries",
+        vegetarian: true,
+        glutenFree: false,
+        price: 3.00,
+        organic: false
+    }
 ];
 	
 
@@ -28,20 +80,42 @@ var products = [
 // given restrictions provided, make a reduced list of products
 // prices should be included in this list, as well as a sort based on price
 
-function restrictListProducts(prods, restriction) {
-	let product_names = [];
+function restrictListProducts(prods, restriction, organic) {
+	let product_list = [];
+
 	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name);
+		if (restriction == "Vegetarian" && prods[i].vegetarian == true && organic == true && prods[i].organic == true) {
+            product_list.push(prods[i]);
 		}
-		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-			product_names.push(prods[i].name);
+        else if (restriction == "Vegetarian" && prods[i].vegetarian == true && organic == false) {
+            product_list.push(prods[i]);
 		}
-		else if (restriction == "None"){
-			product_names.push(prods[i].name);
+		else if (restriction == "GlutenFree" && prods[i].glutenFree == true && organic == true && prods[i].organic == true){
+            product_list.push(prods[i]);			
+		}
+        else if (restriction == "GlutenFree" && prods[i].glutenFree == true && organic == false ){
+            product_list.push(prods[i]);			
+		}
+        else if (restriction == "Both" && (prods[i].vegetarian == true && prods[i].glutenFree == true) && organic == true && prods[i].organic == true){
+            product_list.push(prods[i]);
+        }
+        else if (restriction == "Both" && (prods[i].vegetarian == true && prods[i].glutenFree == true) && organic == false){
+            product_list.push(prods[i]);
+        }
+		else if (restriction == "None" && organic == true && prods[i].organic == true){
+			product_list.push(prods[i]);
+		}
+        else if (restriction == "None" && organic == false){
+			product_list.push(prods[i]);
+		}
+        else if (restriction == "" && organic == true && prods[i].organic == true){
+			product_list.push(prods[i]);
+		}
+        else if (restriction === "" && organic == false){
+			product_list.push(prods[i]);
 		}
 	}
-	return product_names;
+    return product_list;
 }
 
 // Calculate the total price of items, with received parameter being a list of products
--- Module-2-Grocery/scripts/main.js
+++ Module-2-Grocery/scripts/main.js
@@ -27,35 +27,44 @@ function openInfo(evt, tabName) {
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
-function populateListProductChoices(slct1, slct2) {
+function populateListProductChoices(slct1, slct2, slct3) {
     var s1 = document.getElementById(slct1);
     var s2 = document.getElementById(slct2);
-	
+	var s3 = document.getElementById(slct3);
+    
 	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
     s2.innerHTML = "";
 		
 	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
-
+    var optionArray = restrictListProducts(products, s1.value, s3.checked);
+   
+    //sorts the obtain product list by ascending order 
+    optionArray.sort(function(a,b){return a.price-b.price});
+   
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
 	// <label for="Bread">Bread/label><br>
 		
 	for (i = 0; i < optionArray.length; i++) {
 			
-		var productName = optionArray[i];
+		var product = optionArray[i];
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		checkbox.name = "product";
-		checkbox.value = productName;
+		checkbox.value = product.name;
 		s2.appendChild(checkbox);
 		
 		// create a label for the checkbox, and also add in HTML DOM
 		var label = document.createElement('label')
-		label.htmlFor = productName;
-		label.appendChild(document.createTextNode(productName));
+		label.htmlFor = product.name;
+		label.appendChild(document.createTextNode(product.name));
 		s2.appendChild(label);
+        
+        var priceLabel = document.createElement('label');
+        priceLabel.htmlFor = product.name + "-price";
+        priceLabel.appendChild(document.createTextNode(" ($" + product.price +")"))
+        s2.appendChild(priceLabel);
 		
 		// create a breakline node and add in HTML DOM
 		s2.appendChild(document.createElement("br"));    
@@ -88,6 +97,6 @@ function selectedItems(){
 		
 	// add paragraph and total price
 	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts)));
 		
 }
